531db7bfa0796cac4e386cf7f37d9ea1f1b07bb0
Justine Tunney <jtunney@lobstertech.com>
2012-06-01 21:21:21 -0400

    powerline

--- init.el
+++ init.el
@@ -71,6 +71,7 @@
                             (concat dotfiles-dir "vendor")
                             (concat dotfiles-dir "vendor/magit")
                             (concat dotfiles-dir "vendor/yasnippet")
+                            (concat dotfiles-dir "vendor/emacs-powerline")
                             (concat dotfiles-dir "vendor/coffee-mode")))
 (dolist (dir lob/vendor-dirs) (add-to-list 'load-path dir))
 (add-to-list 'custom-theme-load-path (concat dotfiles-dir "themes"))
@@ -154,6 +155,14 @@
 (auto-compression-mode t)
 (delete-selection-mode 1)
 (show-paren-mode 1)
+
+;; make the status line sexy
+(require 'cl)
+(require 'powerline)
+(setq powerline-arrow-shape 'arrow14)
+(custom-set-faces
+ '(mode-line ((t (:foreground "#030303" :background "#bdbdbd" :box nil))))
+ '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil)))))
 
 (require 'saveplace)
 (require 'ffap)
